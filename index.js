783edf1967bd2b86d642f32571cd269e667a6f4e
RISHU RAJ <go23rishu@gmail.com>
2021-05-27 21:10:11 +0530

    Server Backend

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+const { MONGODB } = require("./config");
+const pubsub = new PubSub();
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/typeDefs");
+const PORT = process.env.port || 5000;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`server running at port ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
